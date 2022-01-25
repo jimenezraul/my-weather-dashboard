b14272ae78992be722db67c893c9d7b0491e6770
Raul <jimenezraul1981@gmail.com>
2022-01-25 10:30:31 -0500

    added function to show weather on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,56 @@
 var search = $("#search");
 var searchBtn = $("#search-btn");
 var message = $("#message");
+var weatherCol = $("#weather-col");
+var forecast = $("#forecast");
+var forecastCol = $("#forecast-col");
 var cityHistory = {};
 var apiKey = "2c55cf825b3d6637f09bec8a5d37fed0";
 
 $(document).ready(onLoad());
 
+var showWeather = function (data) {
+  weatherCol.addClass("bg-white border border-dark p-3 rounded");
+  var h2 = `<h2>${data.city} (3/30/2021) <img class='weather-icon btn-history rounded-circle' src='https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png' alt='' /></h2>`;
+  var p = `<p>Temp: ${data.temp}ºF</p>
+    <p>Wind: ${data.wind} MPH</p>
+    <p>Humidity: ${data.humidity} %</p>
+    <p>UV index: <span class="badge bg-success p-2">${data.uvi}</span></p>`;
+  weatherCol.append(h2);
+  weatherCol.append(p);
+};
+
+var showFiveDaysWeather = function (arr) {
+  forecastCol.removeClass("d-none");
+  //create a div for each day
+  arr.forEach((day) => {
+    var text = day.dt_txt.split(" ")[0];
+    text = text.split("-");
+    text = `${text[1]}/${text[2]}/${text[0]}`;
+    var div = $("<div>");
+    div.addClass("rounded col-5 col-lg-3 col-xl-2 offset-xl-0 bg-card p-2 mt-1");
+    // add class to every 2nd div except the first
+    var index = arr.indexOf(day) + 1;
+    if (index % 2 === 0 && index !== 4) {
+      div.addClass("offset-lg-1");
+    } else if (index % 3 === 0) {
+        div.addClass("offset-lg-1");
+    }
+    var h4 = `<h4>${text}</h4>`;
+    var img = `<img class='weather-icon btn-history rounded-circle my-2' src="https://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" alt=""/>`;
+    var p = `<p>Temp: ${day.main.temp}ºF</p>
+        <p>Wind: ${day.wind.speed} MPH</p>
+        <p>Humidity: ${day.main.humidity} %</p>`;
+    div.append(h4);
+    div.append(img);
+    div.append(p);
+    forecast.append(div);
+  });
+};
+
 var errorMessage = function (message) {
-  var p = `<h3><span class="badge bg-danger">${message}</span></h3>`;
-  $("#city-message").append(p);
+  var h3 = `<h3><span class="badge bg-danger">${message}</span></h3>`;
+  $("#city-message").append(h3);
   setTimeout(function () {
     $("#city-message h3").remove();
   }, 3000);
@@ -19,31 +61,90 @@ var saveWeather = function () {
   console.log(cityHistory);
 };
 
+var getFiveDaysWeather = function (city) {
+  var url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+  fetch(url).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        cityHistory.city.forEach(function (weather) {
+          if (weather.city.toUpperCase() === city.toUpperCase()) {
+            var myList = [];
+            data.list.filter((day) => {
+              if (day.dt_txt.endsWith("00:00:00")) {
+                myList.push(day);
+              }
+            });
+            weather.forecast = myList;
+            showFiveDaysWeather(myList);
+          }
+        });
+      });
+    } else {
+      errorMessage(`City ${response.statusText}`);
+    }
+  });
+};
+
+var getUviIndex = async function (coord) {
+  var lat = coord.lat;
+  var lon = coord.lon;
+  var URL = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
+  var res = await fetch(URL);
+  res = await res.json();
+  return res.value;
+};
+
+// remove elements from weatherCol parent
+var resetEl = function () {
+  weatherCol.empty();
+  weatherCol.removeClass("bg-white border border-dark p-3 rounded");
+  forecast.empty();
+  forecastCol.addClass("d-none");
+};
+
 var getWeather = function (city) {
   var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   fetch(url)
-      .then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-              console.log(data)
-            });
-          } else {
-            errorMessage(`City ${response.statusText}`)
-          }
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(async function (data) {
+          var uvi = await getUviIndex(data.coord);
+          var weather = {
+            city: data.name,
+            temp: data.main.temp,
+            humidity: data.main.humidity,
+            weather: data.weather,
+            uvi: uvi,
+            forecast: [],
+            wind: data.wind.speed,
+          };
+          cityHistory.city.push(weather);
+          getFiveDaysWeather(city);
+          showWeather(weather);
+        });
+      } else {
+        errorMessage(`City ${response.statusText}`);
+      }
     })
+    .catch(function (e) {
+      console.log(e);
+    });
 };
 
 var checkWeather = function (city) {
   if (!!cityHistory.city.length) {
     cityHistory.city.forEach((weather) => {
-      console.log(weather.city === city);
+      if (weather.city.toUpperCase() === city.toUpperCase()) {
+        cityHistory.city.splice(cityHistory.city.indexOf(weather), 1);
+      }
     });
   }
   getWeather(city);
   saveWeather();
 };
 
-var searchHandler = function () {
+var searchHandler = function (e) {
+  e.preventDefault();
   var city = search.val().trim();
   if (!city) {
     if ($("#message p").length === 0) {
@@ -55,6 +156,8 @@ var searchHandler = function () {
       return;
     }
   }
+  search.val("");
+  resetEl();
   checkWeather(city);
 };
 
